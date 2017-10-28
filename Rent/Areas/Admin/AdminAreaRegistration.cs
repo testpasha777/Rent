5a9f0fd19dee7818f86475d44b815bf5c134cce7
testpasha777@gmail.com <testpasha777@gmail.com>
2017-10-28 18:24:01 +0300

    added simple design to admin panel

--- Rent/Areas/Admin/AdminAreaRegistration.cs
+++ Rent/Areas/Admin/AdminAreaRegistration.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Rent.Areas.Admin
 {
-    public class AdminAreaRegistration : AreaRegistration
+    public class AdminAreaRegistration : AreaRegistration 
     {
-        public override string AreaName
+        public override string AreaName 
         {
-            get
+            get 
             {
                 return "Admin";
             }
         }
 
-        public override void RegisterArea(AreaRegistrationContext context)
+        public override void RegisterArea(AreaRegistrationContext context) 
         {
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                );
+                new { action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }

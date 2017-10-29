9e5cbd8ce28626cf80aa4edf28269abcdb03e4b7
testpasha777@gmail.com <testpasha777@gmail.com>
2017-10-29 16:06:50 +0200

    Create dbcontext and identity table

---
+++ DAL/Entities/EFContext.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Entities.Identity;
+using DAL.Interface;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class EFContext : IdentityDbContext<AppUser>, IEFContext
+    {
+        public EFContext() : base("RentConnection")
+        {
+            Database.SetInitializer<EFContext>(null);
+        }
+
+        public EFContext(string connString) : base(connString)
+        {
+            Database.SetInitializer<EFContext>(new DBInitializer());
+        }
+
+        public static EFContext Create()
+        {
+            return new EFContext();
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            return base.Set<TEntity>();
+        }
+    }
+}

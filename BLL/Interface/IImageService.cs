ac07b49049bc2fae881785ce7e8d90e592562c46
testpasha777@gmail.com <testpasha777@gmail.com>
2017-11-06 16:01:56 +0200

    Create image service

---
+++ BLL/Interface/IImageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BLL.Interface
+{
+    public interface IImageService
+    {
+        Bitmap CreateImage(HttpPostedFileBase image, int maxWidth, int maxHeight);
+    }
+}
---
+++ BLL/Services/ImageService.cs
@@ -0,0 +1,58 @@
+﻿using BLL.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Web;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace BLL.Services
+{
+    public class ImageService : IImageService
+    {
+
+        public ImageService()
+        {
+            
+        }
+
+        public Bitmap CreateImage(HttpPostedFileBase image, int maxWidth, int maxHeight)
+        {
+            if (image != null && image.ContentLength != 0)
+            {
+                try
+                {
+                    using (Bitmap originalPic = new Bitmap(image.InputStream, true))
+                    {
+                        int originalWidth = originalPic.Width;
+                        int originalHeight = originalPic.Height;
+                        // To preserve the aspect ratio
+                        float ratioX = (float)maxWidth / (float)originalWidth;
+                        float ratioY = (float)maxHeight / (float)originalHeight;
+                        float ratio = Math.Min(ratioX, ratioY);
+                        // New width and height based on aspect ratio
+                        int width = (int)(originalWidth * ratio);
+                        int height = (int)(originalHeight * ratio);
+
+                        using (Bitmap outBmp = new Bitmap(width, height, PixelFormat.Format24bppRgb))
+                        {
+                            using (Graphics oGraphics = Graphics.FromImage(outBmp))
+                            {
+                                oGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                                oGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                                oGraphics.DrawImage(originalPic, 0, 0, width, height);
+                                return new Bitmap(outBmp);
+                            }
+                        }
+                    }
+                }
+                catch { }
+            }
+
+            return null;
+        }
+    }
+}
